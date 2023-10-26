9a6c0d26fd5b71f1f834a56117e48e0714bdec5e
matix0 <mateusviniciuspw@gmail.com>
2023-10-25 22:04:35 -0300

    finish tests

--- backend/src/tests/app.test.js
+++ backend/src/tests/app.test.js
@@ -10,6 +10,15 @@ const user = {
   favorite: false,
 };
 
+const wrongUser = {
+  id: 61623585,
+  name: "Matix",
+  username: "matix0",
+  avatar_url: "https://avatars.githubusercontent.com/u/61623585?v=4",
+  html_url: "https://github.com/matix0",
+  favorite: false,
+};
+
 describe("Teste de create", () => {
   it("deve conseguir cadastrar um usuario", async () => {
     const res = await request(app).post("/users").send(user);
@@ -21,6 +30,10 @@ describe("Teste de create", () => {
     expect(res.statusCode).toEqual(400);
   });
 
+  it("nao deve conseguir cadastrar um usuario inexistente no github", async () => {
+    const res = await request(app).post("/users").send(wrongUser);
+    expect(res.statusCode).toEqual(400);
+  });
 });
 
 describe("Teste de read", () => {
@@ -28,6 +41,22 @@ describe("Teste de read", () => {
     const res = await request(app).get("/users");
     expect(res.statusCode).toEqual(200);
   });
-
 });
 
+describe("Teste de deletar", () => {
+  it("deve conseguir deletar um usuario", async () => {
+    const res = await request(app).delete(`/users/${user.username}`);
+    expect(res.statusCode).toEqual(200);
+  });
+  it("nao deve conseguir deletar um usuario que nao esteja cadastrado", async () => {
+    const res = await request(app).delete(`/users/${wrongUser.username}`);
+    expect(res.statusCode).toEqual(400);
+  });
+});
+
+describe("Teste de favoritar", () => {
+  it("deve conseguir favoritar um usuario", async () => {
+    const res = await request(app).patch(`/users/${user.username}`);
+    expect(res.statusCode).toEqual(200);
+  });
+});
